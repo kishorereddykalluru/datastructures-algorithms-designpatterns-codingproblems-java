b59d010686d61e06891285825a039bb4c5dea277
Kishore Reddy <kishorereddykalluru@gmail.com>
2021-01-09 21:48:21 +0530

    Remove duplicates from List and remove duplicates from another that matche current list

---
+++ src/javalearning/generics/java8/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package javalearning.generics.java8;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        List<String> li = List.of("abc", "xyz", "lmn", "nop", "qrs", "abc", "mno", "qrs", "tuv");
+
+        List<String> li1 = List.of("abc", "xyz", "lmn", "acv", "bcv", "dcv", "zcv", "accv",
+                "aecv", "acfv", "acgv", "acav", "ascv", "acv");
+
+        List<String> collect = li1.stream().filter(x -> !li.contains(x)).collect(Collectors.toList());
+
+        System.out.println("collect.size() = " + collect.size() + " <-----> li1.size() = "+li1.size());
+
+        collect.forEach(System.out::println);
+    }
+}
