b16486be7c9f4586d1a0a14086d3d4cb04b78810
Kishore Reddy <kishorereddykalluru@gmail.com>
2021-01-10 20:49:11 +0530

    recurssion and mergesort

---
+++ src/algorithms/sorting/MergeSort.java
@@ -0,0 +1,49 @@
+package algorithms.sorting;
+
+import java.rmi.dgc.VMID;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        mergeSort(intArray, 0, intArray.length);
+
+        for(int num: intArray){
+            System.out.println("num = " + num);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int start, int end){
+
+        if(end - start < 2)
+            return;
+
+        int mid = (start+end)/2;
+
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid, end);
+        merge(arr, start, mid, end);
+    }
+
+    public static void merge(int[] array, int start, int mid, int end){
+
+        if(array[mid-1] <= array[mid])
+            return;
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end-start];
+
+        while(i < mid && j < end)
+        {
+            temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+        }
+
+        System.arraycopy(array, i, array, start+tempIndex, mid-i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+    }
+}
---
+++ src/algorithms/sorting/Recursion.java
@@ -0,0 +1,18 @@
+package algorithms.sorting;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("factorial = " + recursiveFactorial(15));
+        
+    }
+    
+    private static int recursiveFactorial(int num){
+        if(num == 0)
+            return 1;
+        
+        return num * recursiveFactorial(num - 1);
+    }
+}
