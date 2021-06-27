def5c0e7957e70502b685994efeb65ebdf8c3b11
Kishore Reddy <kishorereddykalluru@gmail.com>
2021-06-27 18:31:05 +0530

    cracked digit88 coding challenge

---
+++ src/javaprogramming/collections/list/ListDemo.java
@@ -0,0 +1,9 @@
+package javaprogramming.collections.list;
+
+public class ListDemo {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/javaprogramming/collections/set/SetDemo.java
+++ src/javaprogramming/collections/set/SetDemo.java
@@ -6,16 +6,19 @@ import java.util.Set;
 public class SetDemo {
 
     public static void main(String[] args) {
-        int arr[] = {10, 5, 5, 5, 10, 5, 3, 4, 1, 6, 7, 8, 9};
+        int arr[] = {10, 5, 5, 5, 10, 3, 4, 1, 6, 7, 8, 9, 4, 11};
         int target = 15;
 
-        Set<Pairs> pairs = new HashSet<>();
-
-        for(int i: arr){
-            int result = target - i;
-            pairs.add(new Pairs(i, result));
+        Set<Pairs> result = new HashSet<>();
+        Set<Integer> pairSet = new HashSet<>();
+        for(int i : arr){
+            int negate = target - i;
+            if(pairSet.contains(negate)) {
+                result.add(new Pairs(i, negate));
+            } else {
+                pairSet.add(i);
+            }
         }
-        System.out.println(pairs.size());
-        System.out.println("pairs = " + pairs);
+        result.forEach(System.out::println);
     }
 }
