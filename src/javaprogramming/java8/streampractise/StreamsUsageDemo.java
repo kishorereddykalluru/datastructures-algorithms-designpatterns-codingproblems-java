eeef8bfc5049d4d2518c2a47f9ce2aff13943ddd
Kishore Reddy <kishorereddykalluru@gmail.com>
2021-07-01 14:16:39 +0530

    streams to find average of numbers

---
+++ src/javaprogramming/java8/streampractise/StreamsUsageDemo.java
@@ -0,0 +1,16 @@
+package javaprogramming.java8.streampractise;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamsUsageDemo {
+    public static void main(String[] args) {
+
+        List<Integer> nums = List.of(1,2,3,4,5,6,7,8);
+
+        double average = nums.stream().mapToInt(i -> i).average().getAsDouble();
+        System.out.println("Average = " + average);
+
+        
+    }
+}
